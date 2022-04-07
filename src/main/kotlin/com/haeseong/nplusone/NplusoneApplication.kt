89bce71a4750c918f1ef506e39ca526accea5e52
Haeseong Jeon <seong0428@gmail.com>
2022-04-07 22:10:26 +0900

    Initial Commit

---
+++ src/main/kotlin/com/haeseong/nplusone/NplusoneApplication.kt
@@ -0,0 +1,11 @@
+package com.haeseong.nplusone
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NplusoneApplication
+
+fun main(args: Array<String>) {
+	runApplication<NplusoneApplication>(*args)
+}
