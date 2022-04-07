f5703f286d22a6721baa57801e667bc3d886349c
Haeseong Jeon <seong0428@gmail.com>
2022-04-07 22:14:52 +0900

    feat: selenium 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,8 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.seleniumhq.selenium:selenium-java:4.1.3")
+	implementation("io.github.bonigarcia:webdrivermanager:5.1.0")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	runtimeOnly("com.h2database:h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
