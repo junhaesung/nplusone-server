016b9098d1856633c8832335cf25c0936c4e9446
Haeseong Jeon <seong0428@gmail.com>
2022-04-10 12:12:35 +0900

    docs: swagger 추가

--- nplusone-api/build.gradle.kts
+++ nplusone-api/build.gradle.kts
@@ -4,6 +4,7 @@ dependencies {
     implementation(project(":nplusone-domain"))
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("io.springfox:springfox-boot-starter:3.0.0")
 }
 
 tasks.withType<Jar> {
