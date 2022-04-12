74a6644e051735181ae284976714507deab49103
Haeseong Jeon <seong0428@gmail.com>
2022-04-12 23:16:48 +0900

    config: jasypt 추가

--- nplusone-domain/build.gradle.kts
+++ nplusone-domain/build.gradle.kts
@@ -4,6 +4,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("com.h2database:h2")
     implementation("mysql:mysql-connector-java")
+    implementation("com.github.ulisesbocchio:jasypt-spring-boot-starter:3.0.4")
 }
 
 tasks.withType<Jar> {
