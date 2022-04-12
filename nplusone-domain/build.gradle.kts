49185181fef201695f33043bf3d558296db0a2e4
Haeseong Jeon <seong0428@gmail.com>
2022-04-13 01:00:04 +0900

    config: h2 test 에서 사용하도록 변경

--- nplusone-domain/build.gradle.kts
+++ nplusone-domain/build.gradle.kts
@@ -2,9 +2,9 @@ import org.springframework.boot.gradle.tasks.bundling.BootJar
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("com.h2database:h2")
     implementation("mysql:mysql-connector-java")
     implementation("com.github.ulisesbocchio:jasypt-spring-boot-starter:3.0.4")
+    testImplementation("com.h2database:h2")
 }
 
 tasks.withType<Jar> {
