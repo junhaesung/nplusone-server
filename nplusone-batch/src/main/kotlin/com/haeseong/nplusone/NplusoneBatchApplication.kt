98d025de7365cb8486b7cc60813948abd24a2502
Haeseong Jeon <seong0428@gmail.com>
2022-04-13 00:04:03 +0900

    config: batch 설정 추가

--- nplusone-batch/src/main/kotlin/com/haeseong/nplusone/NplusoneBatchApplication.kt
+++ nplusone-batch/src/main/kotlin/com/haeseong/nplusone/NplusoneBatchApplication.kt
@@ -2,11 +2,12 @@ package com.haeseong.nplusone
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import java.time.LocalDateTime
 
 @SpringBootApplication
 class NplusoneBatchApplication
 
 fun main(args: Array<String>) {
     System.setProperty("spring.config.name", "application,batch")
-    SpringApplication.run(NplusoneBatchApplication::class.java, *args)
+    SpringApplication.run(NplusoneBatchApplication::class.java, *(args + "startTime=${LocalDateTime.now()}"))
 }
---
+++ nplusone-batch/src/main/kotlin/com/haeseong/nplusone/infrastructure/BatchConfig.kt
@@ -0,0 +1,12 @@
+package com.haeseong.nplusone.infrastructure
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing
+import org.springframework.context.annotation.Configuration
+
+@EnableBatchProcessing
+@Configuration
+class BatchConfig {
+    companion object {
+        const val SPRING_BATCH_JOB_NAMES = "spring.batch.job.names"
+    }
+}
