159cf31c1d8d7672d5f071e2b9535edaba508483
Haeseong Jeon <seong0428@gmail.com>
2022-04-13 00:04:39 +0900

    feat: cu collector job 추가

---
+++ nplusone-batch/src/main/kotlin/com/haeseong/nplusone/job/collector/cu/CuCollectorConfig.kt
@@ -0,0 +1,52 @@
+package com.haeseong.nplusone.job.collector.cu
+
+import com.haeseong.nplusone.infrastructure.BatchConfig
+import org.springframework.batch.core.Job
+import org.springframework.batch.core.Step
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
+import org.springframework.batch.core.configuration.annotation.JobScope
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory
+import org.springframework.batch.core.configuration.annotation.StepScope
+import org.springframework.batch.core.repository.JobRepository
+import org.springframework.batch.support.transaction.ResourcelessTransactionManager
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@ConditionalOnProperty(
+    value = [BatchConfig.SPRING_BATCH_JOB_NAMES],
+    havingValue = CuCollectorConfig.JOB_NAME
+)
+@Configuration
+class CuCollectorConfig(
+    private val jobBuilderFactory: JobBuilderFactory,
+    private val jobRepository: JobRepository,
+    private val stepBuilderFactory: StepBuilderFactory,
+) {
+    @Bean
+    fun cuCollectorJob(): Job {
+        return jobBuilderFactory[JOB_NAME]
+            .repository(jobRepository)
+            .start(cuCollectorStep())
+            .build()
+    }
+
+    @Bean
+    @JobScope
+    fun cuCollectorStep(): Step {
+        return stepBuilderFactory[STEP_NAME]
+            .tasklet(cuCollectorTasklet())
+            .transactionManager(ResourcelessTransactionManager())
+            .build()
+    }
+
+    @Bean
+    @StepScope
+    fun cuCollectorTasklet() = CuCollectorTasklet()
+
+    companion object {
+        const val JOB_NAME = "cu-collector-job"
+        const val STEP_NAME = "cu-collector-job-step"
+    }
+}
+
