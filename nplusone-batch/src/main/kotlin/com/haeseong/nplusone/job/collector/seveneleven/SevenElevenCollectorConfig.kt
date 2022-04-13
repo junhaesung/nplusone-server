d1adea0718edebab6509137b4f7e796bb96ac633
Haeseong Jeon <seong0428@gmail.com>
2022-04-14 01:10:16 +0900

    feat: seven eleven collector job 추가

---
+++ nplusone-batch/src/main/kotlin/com/haeseong/nplusone/job/collector/seveneleven/SevenElevenCollectorConfig.kt
@@ -0,0 +1,52 @@
+package com.haeseong.nplusone.job.collector.seveneleven
+
+import com.haeseong.nplusone.infrastructure.BatchConfig
+import com.haeseong.nplusone.job.collector.ministop.MinistopCollectorConfig
+import org.springframework.batch.core.Job
+import org.springframework.batch.core.Step
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
+import org.springframework.batch.core.configuration.annotation.JobScope
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory
+import org.springframework.batch.core.configuration.annotation.StepScope
+import org.springframework.batch.core.repository.JobRepository
+import org.springframework.batch.support.transaction.ResourcelessTransactionManager
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@ConditionalOnProperty(
+    value = [BatchConfig.SPRING_BATCH_JOB_NAMES],
+    havingValue = SevenElevenCollectorConfig.JOB_NAME
+)
+@Configuration
+class SevenElevenCollectorConfig(
+    private val jobBuilderFactory: JobBuilderFactory,
+    private val jobRepository: JobRepository,
+    private val stepBuilderFactory: StepBuilderFactory,
+) {
+    @Bean
+    fun sevenElevenCollectorJob(): Job {
+        return jobBuilderFactory[JOB_NAME]
+            .repository(jobRepository)
+            .start(sevenElevenCollectorStep())
+            .build()
+    }
+
+    @Bean
+    @JobScope
+    fun sevenElevenCollectorStep(): Step {
+        return stepBuilderFactory[STEP_NAME]
+            .tasklet(sevenElevenCollectorTasklet())
+            .transactionManager(ResourcelessTransactionManager())
+            .build()
+    }
+
+    @Bean
+    @StepScope
+    fun sevenElevenCollectorTasklet() = SevenElevenCollectorTasklet()
+
+    companion object {
+        const val JOB_NAME = "seven-eleven-collector-job"
+        const val STEP_NAME = "seven-eleven-collector-step"
+    }
+}
