14c55cf9b64b2343f09d3fff45faf1d72aa42d6e
Haeseong Jeon <seong0428@gmail.com>
2022-04-14 01:08:56 +0900

    feat: ministop 2+1 추가

--- nplusone-batch/src/main/kotlin/com/haeseong/nplusone/job/collector/ministop/MinistopCollectorTasklet.kt
+++ nplusone-batch/src/main/kotlin/com/haeseong/nplusone/job/collector/ministop/MinistopCollectorTasklet.kt
@@ -34,6 +34,10 @@ open class MinistopCollectorTasklet : Tasklet, DiscountedItemValidator {
     }
 
     private fun getDiscountedItems(): List<DiscountedItem> {
+        return getOnePlusOneItems() + getTwoPlusOneItems()
+    }
+
+    private fun getOnePlusOneItems(): List<DiscountedItem> {
         WebDriverManager.chromedriver().setup()
         val chromeOptions = ChromeOptions()
         chromeOptions.setExperimentalOption("excludeSwitches", listOf("disable-popup-blocking"));
@@ -76,7 +80,58 @@ open class MinistopCollectorTasklet : Tasklet, DiscountedItemValidator {
 
                 sequence += 1
             }
-            log.info("items: $items")
+            log.info("items(1+1): $items")
+            return items
+        } finally {
+            // End the session
+            driver.quit()
+        }
+    }
+
+    private fun getTwoPlusOneItems(): List<DiscountedItem> {
+        WebDriverManager.chromedriver().setup()
+        val chromeOptions = ChromeOptions()
+        chromeOptions.setExperimentalOption("excludeSwitches", listOf("disable-popup-blocking"))
+        chromeOptions.setHeadless(true)
+
+        // Start the session
+        val driver = ChromeDriver(chromeOptions)
+        driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS)
+
+        val items = mutableListOf<DiscountedItem>()
+
+        try {
+            driver.get("https://www.ministop.co.kr/MiniStopHomePage/page/event/plus2.do")
+            Thread.sleep(1000)
+
+            // 20개씩 저장
+            var sequence = 1
+            while (true) {
+                val target = try {
+                    driver.findElementByCssSelector("#section > div.inner.wrap.service1 > div.event_plus_list > ul > li:nth-child($sequence)")
+                } catch (e: NoSuchElementException) {
+                    break
+                }
+
+                val split = target.findElement(By.cssSelector("a > p")).text.split("\n").map { it.trim() }
+                val discountedItem = DiscountedItem(
+                    split[0],
+                    split[1].replace(Regex("[,원\\s]"), "").toBigDecimal(),
+                    target.findElement(By.cssSelector("a > img")).getAttribute("src"),
+                    DiscountType.parse(target.findElement(By.cssSelector("a > span")).text.trim()),
+                )
+                items.add(discountedItem)
+
+                if (sequence % 20 == 0) {
+                    // see more
+                    val seeMoreButton = driver.findElementByCssSelector("#section > div.inner.wrap.service1 > div.event_plus_list > div > a.pr_more")
+                    seeMoreButton.click()
+                    Thread.sleep(1000)
+                }
+
+                sequence += 1
+            }
+            log.info("items(2+1): $items")
             return items
         } finally {
             // End the session
