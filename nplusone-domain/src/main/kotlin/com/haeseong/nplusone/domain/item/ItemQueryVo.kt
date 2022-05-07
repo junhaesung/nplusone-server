0da610a9fcd0aba8ccd899454113702ac9dddbb9
전해성 <seong0428@gmail.com>
2022-05-07 13:34:38 +0900

    feat: 목록 조회시 name 조건 추가 (#4)

--- nplusone-domain/src/main/kotlin/com/haeseong/nplusone/domain/item/ItemQueryVo.kt
+++ nplusone-domain/src/main/kotlin/com/haeseong/nplusone/domain/item/ItemQueryVo.kt
@@ -1,6 +1,7 @@
 package com.haeseong.nplusone.domain.item
 
 data class ItemQueryVo(
+    val name: String?,
     val discountType: DiscountType?,
     val storeType: StoreType?,
     val offsetId: Long,
