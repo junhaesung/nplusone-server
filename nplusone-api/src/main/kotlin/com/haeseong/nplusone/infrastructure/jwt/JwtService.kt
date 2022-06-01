2983023f08dfc5f955acc9469d384552e81cd991
Haeseong Jeon <seong0428@gmail.com>
2022-06-01 19:29:48 +0900

    feat: jwt 추가

--- nplusone-api/src/main/kotlin/com/haeseong/nplusone/infrastructure/jwt/JwtService.kt
+++ nplusone-api/src/main/kotlin/com/haeseong/nplusone/infrastructure/jwt/JwtService.kt
@@ -1,15 +1,41 @@
 package com.haeseong.nplusone.infrastructure.jwt
 
+import com.auth0.jwt.JWT
+import com.auth0.jwt.JWTVerifier
+import com.auth0.jwt.algorithms.Algorithm
+import com.auth0.jwt.exceptions.JWTVerificationException
 import com.haeseong.nplusone.application.TokenService
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 
 @Service
-class JwtService : TokenService<Long> {
+class JwtService(
+    @Value("\${jwt.secretKey}")
+    private val secretKey: String,
+) : TokenService<Long> {
+
+    private val algorithm: Algorithm = Algorithm.HMAC256(secretKey)
+    private var jwtVerifier: JWTVerifier = JWT.require(algorithm).build()
+
     override fun encode(memberId: Long): String {
-        return ""
+        return JWT.create()
+            .withClaim(CLAIM_NAME_MEMBER_ID, memberId)
+            .sign(algorithm)
     }
 
     override fun decode(token: String): Long {
-        return 0L
+        return try {
+            jwtVerifier.verify(token).getClaim(CLAIM_NAME_MEMBER_ID).asLong()
+        } catch (e: JWTVerificationException) {
+            log.warn("Failed to decode jwt. token: {}", token, e)
+            throw e
+        }
+    }
+
+    companion object {
+        private const val CLAIM_NAME_MEMBER_ID = "memberId"
+        private val log: Logger = LoggerFactory.getLogger(JwtService::class.java)
     }
 }
