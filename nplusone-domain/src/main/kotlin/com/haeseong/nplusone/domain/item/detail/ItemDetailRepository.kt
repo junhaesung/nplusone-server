6a69d5e099b9345926b34f1029a8d099eeae4f88
Haeseong Jeon <seong0428@gmail.com>
2022-06-09 22:13:06 +0900

    feat: item detail 추가

---
+++ nplusone-domain/src/main/kotlin/com/haeseong/nplusone/domain/item/detail/ItemDetailRepository.kt
@@ -0,0 +1,6 @@
+package com.haeseong.nplusone.domain.item.detail
+
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface ItemDetailRepository : JpaRepository<ItemDetail, Long> {
+}
