da6aa717b6e8909f135885e723b210f43c1a5c26
Haeseong Jeon <seong0428@gmail.com>
2022-06-12 11:03:20 +0900

    feat: wishItem 추가, 삭제기능 구현

---
+++ nplusone-api/src/main/kotlin/com/haeseong/nplusone/ui/item/wish/WishItemController.kt
@@ -0,0 +1,45 @@
+package com.haeseong.nplusone.ui.item.wish
+
+import com.haeseong.nplusone.domain.item.wish.WishItemService
+import com.haeseong.nplusone.ui.ApiResponse
+import org.springframework.security.core.Authentication
+import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/api/v1/wish-items")
+class WishItemController(
+    private val wishItemService: WishItemService,
+) {
+    @PostMapping
+    fun add(
+        authentication: Authentication,
+        @RequestBody wishItemCreateRequest: WishItemCreateRequest,
+    ): ApiResponse<*> {
+        wishItemService.add(
+            memberId = resolveMemberId(authentication),
+            itemId = wishItemCreateRequest.itemId
+        )
+        return ApiResponse.success()
+    }
+
+    @DeleteMapping("/{itemId}")
+    fun remove(
+        authentication: Authentication,
+        @PathVariable itemId: Long,
+    ): ApiResponse<*> {
+        wishItemService.remove(
+            memberId = resolveMemberId(authentication),
+            itemId = itemId,
+        )
+        return ApiResponse.success()
+    }
+
+
+    private fun resolveMemberId(authentication: Authentication): Long {
+        if (authentication is PreAuthenticatedAuthenticationToken) {
+            return authentication.name.toLong()
+        }
+        throw IllegalArgumentException()
+    }
+}
