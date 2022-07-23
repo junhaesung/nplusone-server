9e970c22c0020e4accc5d1ce42c23345fb3f64d3
Haeseong Jeon <seong0428@gmail.com>
2022-07-23 18:06:55 +0900

    fix: endpoint 설정

--- nplusone-api/src/main/kotlin/com/haeseong/nplusone/ui/recommend/RecommendController.kt
+++ nplusone-api/src/main/kotlin/com/haeseong/nplusone/ui/recommend/RecommendController.kt
@@ -8,10 +8,12 @@ import com.haeseong.nplusone.ui.item.toDto
 import org.springframework.security.core.Authentication
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import springfox.documentation.annotations.ApiIgnore
 
-@RestController("/api/v1/recommend")
+@RestController
+@RequestMapping("/api/v1/recommend")
 class RecommendController(
     private val recommendService: RecommendService,
 ) {
