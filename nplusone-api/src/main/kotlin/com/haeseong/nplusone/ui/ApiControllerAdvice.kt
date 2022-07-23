d55a4367a4da2aa2c040b8b5d66b7b0a7b08701b
전해성 <seong0428@gmail.com>
2022-07-23 18:01:22 +0900

    Merge pull request #28
    * feat: BadRequest 대상 exception 추가

--- nplusone-api/src/main/kotlin/com/haeseong/nplusone/ui/ApiControllerAdvice.kt
+++ nplusone-api/src/main/kotlin/com/haeseong/nplusone/ui/ApiControllerAdvice.kt
@@ -3,15 +3,19 @@ package com.haeseong.nplusone.ui
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
+import org.springframework.http.converter.HttpMessageNotReadableException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
 
 @RestControllerAdvice
 class ApiControllerAdvice {
-    @ExceptionHandler(BadRequestException::class)
+    @ExceptionHandler(
+        BadRequestException::class,
+        HttpMessageNotReadableException::class,
+    )
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    fun handleBadRequestException(e: BadRequestException): ApiResponse<*> {
+    fun handleBadRequestException(e: RuntimeException): ApiResponse<*> {
         log.warn("BAD_REQUEST", e)
         return ApiResponse.failure()
     }
