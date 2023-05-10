cb3d36816c1b56489799a3d0cd86a4cb40369f64
Haeseong Jeon <seong0428@gmail.com>
2023-05-10 18:17:19 +0900

    feat: uuid 로그인 추가

--- nplusone-api/src/test/kotlin/com/haeseong/nplusone/ui/member/MemberControllerTest.kt
+++ nplusone-api/src/test/kotlin/com/haeseong/nplusone/ui/member/MemberControllerTest.kt
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.haeseong.nplusone.application.TokenService
 import com.haeseong.nplusone.domain.member.IdProviderType
 import com.haeseong.nplusone.infrastructure.firebase.FirebaseConfig
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.mockito.Mockito.`when`
 import org.mockito.Mockito.anyLong
@@ -15,6 +16,7 @@ import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import java.util.*
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -31,12 +33,30 @@ class MemberControllerTest {
     @MockBean
     lateinit var firebaseConfig: FirebaseConfig
 
+    @DisplayName("login 성공: FIREBASE")
     @Test
-    fun login() {
+    fun login_firebase() {
         // given
         val loginRequestDto = LoginRequestDto(
             idProviderType = IdProviderType.FIREBASE,
             idProviderUserId = "idProviderUserId",
+        )
+        `when`(tokenService.encode(anyLong())).thenReturn("accessToken")
+        // when
+        mockMvc.perform(post("/api/v1/members/login")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsBytes(loginRequestDto)))
+            // then
+            .andExpect(status().isOk)
+    }
+
+    @DisplayName("login 성공: UUID")
+    @Test
+    fun login_uuid() {
+        // given
+        val loginRequestDto = LoginRequestDto(
+            idProviderType = IdProviderType.UUID,
+            idProviderUserId = UUID.randomUUID().toString(),
         )
         `when`(tokenService.encode(anyLong())).thenReturn("accessToken")
         // when
